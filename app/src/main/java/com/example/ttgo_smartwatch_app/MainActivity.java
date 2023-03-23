9bcda4038e6b33aa26da7684bf8681cc71723e32
Artjoms Alijevs <artjoms.alijevs@gmail.com>
2023-03-23 20:36:47 +0200

    BT logic (in progress)

--- app/src/main/java/com/example/ttgo_smartwatch_app/MainActivity.java
+++ app/src/main/java/com/example/ttgo_smartwatch_app/MainActivity.java
@@ -1,8 +1,28 @@
 package com.example.ttgo_smartwatch_app;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +31,110 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.your_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.connect_to_bt:
+                showConnectionDialog();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showConnectionDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Connect to watch?")
+                .setMessage("Connect to 2gdadfdas watch?")
+                .setPositiveButton("Connect", (dialog, which) -> {
+                    tryToConnect();
+                })
+                //.setNegativeButton(android.R.string.no, null)
+                //.setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+
+    private static final UUID BT_MODULE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private BluetoothAdapter mBTAdapter;
+    private BluetoothSocket mBTSocket = null;
+
+    public final static int MESSAGE_READ = 2;
+    private final static int CONNECTING_STATUS = 3;
+
+    private ConnectedThread mConnectedThread;
+
+    private Handler mHandler;
+
+    private void tryToConnect() {
+
+        mHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == MESSAGE_READ) {
+                    String readMessage = null;
+                    readMessage = new String((byte[]) msg.obj, StandardCharsets.UTF_8);
+                    //mReadBuffer.setText(readMessage);
+                }
+            }
+        };
+
+        mBTAdapter = BluetoothAdapter.getDefaultAdapter();
+        new Thread() {
+            @Override
+            public void run() {
+                boolean fail = false;
+
+                BluetoothDevice device = mBTAdapter.getRemoteDevice("MA:C0:AD:DR:ES:S0");
+
+                try {
+                    mBTSocket = createBluetoothSocket(device);
+                } catch (IOException e) {
+                    fail = true;
+                    Toast.makeText(getBaseContext(), "ERROR", Toast.LENGTH_SHORT).show();
+                }
+                // Establish the Bluetooth socket connection.
+                try {
+                    mBTSocket.connect();
+                } catch (IOException e) {
+                    try {
+                        fail = true;
+                        mBTSocket.close();
+                        mHandler.obtainMessage(CONNECTING_STATUS, -1, -1)
+                                .sendToTarget();
+                    } catch (IOException e2) {
+                        //insert code to deal with this
+                        Toast.makeText(getBaseContext(), "ERROR", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                if(!fail) {
+                    mConnectedThread = new ConnectedThread(mBTSocket, mHandler);
+                    mConnectedThread.start();
+
+                    mHandler.obtainMessage(CONNECTING_STATUS, 1, -1, "my watch")
+                            .sendToTarget();
+                }
+            }
+        }.start();
+    }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        try {
+            final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", UUID.class);
+            return (BluetoothSocket) m.invoke(device, BT_MODULE_UUID);
+        } catch (Exception e) {
+            Log.e("TAG", "Could not create Insecure RFComm Connection",e);
+        }
+        return device.createRfcommSocketToServiceRecord(BT_MODULE_UUID);
+    }
+
+
 }

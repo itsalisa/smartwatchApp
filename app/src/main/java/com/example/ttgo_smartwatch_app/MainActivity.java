1dc7bd948b10ae221f249df5972fa4f9b8228ab1
itsalisa <alisaalijeva123@gmail.com>
2023-04-07 01:00:58 +0100

    battery percentage chart added

--- app/src/main/java/com/example/ttgo_smartwatch_app/MainActivity.java
+++ app/src/main/java/com/example/ttgo_smartwatch_app/MainActivity.java
@@ -25,6 +25,7 @@ import com.anychart.enums.Anchor;
 import com.anychart.enums.HoverMode;
 import com.anychart.enums.Position;
 import com.anychart.enums.TooltipPositionMode;
+import com.anychart.graphics.vector.Stroke;
 import com.example.ttgo_smartwatch_app.database.DatabaseManager;
 import com.example.ttgo_smartwatch_app.database.entity.Date;
 import com.example.ttgo_smartwatch_app.database.entity.Location;
@@ -78,6 +79,8 @@ public class MainActivity extends AppCompatActivity {
     private void setupViews() {
         setupFirstChart();
         setupSecondChart();
+        setupThirdChart();
+        setupFourthChart();
         runOnBackground(() -> {
             List<Movement> movements = databaseManager.dao.getAllMovements();
             List<Date> dates = databaseManager.dao.getAllDates();
@@ -181,7 +184,7 @@ public class MainActivity extends AppCompatActivity {
                 int hour = calendar.get(Calendar.HOUR_OF_DAY);
                 float steps = movement.StepCounter;
                 if (stepsByHour.containsKey(hour)) {
-                    steps += stepsByHour.get(hour);
+                    steps = stepsByHour.get(hour);
                 }
                 stepsByHour.put(hour, steps);
             }
@@ -234,6 +237,72 @@ public class MainActivity extends AppCompatActivity {
                 anyChartView.setChart(cartesian);
             });
         });
+    }
+
+    private void setupThirdChart() {
+
+
+    }
+
+    private void setupFourthChart() {
+        runOnBackground(() -> {
+        List<Movement> movements = databaseManager.dao.getAllMovements();
+
+        // Group by hour
+        HashMap<Integer, Integer> batteryByHour = new HashMap<>();
+        for (Movement movement : movements) {
+            long timestamp = movement.timeStamp;
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(timestamp);
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            int battery = movement.battery;
+            if (batteryByHour.containsKey(hour)) {
+                battery = batteryByHour.get(hour);
+            }
+            batteryByHour.put(hour, battery);
+        }
+
+        // Initialising lists for the data
+        ArrayList<DataEntry> batteryData = new ArrayList<>();
+
+        // Going through the list and populating data list
+        for (int hour = 0; hour < 24; hour++) {
+            int battery = batteryByHour.containsKey(hour) ? batteryByHour.get(hour) : 0;
+            // Adding new data entry list
+            batteryData.add(new ValueDataEntry(String.valueOf(hour), battery));
+        }
+        // Creating chart
+        Cartesian batteryChart = AnyChart.cartesian();
+        batteryChart.animation(true);
+        batteryChart.padding(10d, 20d, 5d, 20d);
+        batteryChart.crosshair().enabled(true);
+        batteryChart.crosshair()
+                .yLabel(true)
+                .yStroke((Stroke) null, null, null, (String) null, (String) null);
+        batteryChart.tooltip().positionMode(TooltipPositionMode.POINT);
+        batteryChart.title("Battery percentage over time");
+
+        Set batteryDataSet = Set.instantiate();
+        batteryDataSet.data(batteryData);
+        Mapping batteryMapping = batteryDataSet.mapAs("{ x: 'x', value: 'value' }");
+
+        Line batteryLine = batteryChart.line(batteryMapping);
+        batteryLine.name("Battery percentage");
+
+        batteryChart.legend().enabled(true);
+        batteryChart.legend().fontSize(13d);
+        batteryChart.legend().padding(0d, 0d, 10d, 0d);
+
+        batteryChart.yAxis(0).title("%");
+        batteryChart.xAxis(0).title("Hour");
+
+        // Display the chart
+        AnyChartView batteryChartView = findViewById(R.id.any_chart_view);
+            runOnUiThread(() -> {
+                batteryChartView.setChart(batteryChart);
+            });
+        });
+
     }
 
     private void showConnectionDialog() {
